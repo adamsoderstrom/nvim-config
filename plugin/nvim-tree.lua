180713ed5c9293a8596990f80da86004a969494f
Martin Barri <maeertin@gmail.com>
2022-11-30 22:52:00 +0100

    update nvim-tree config

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,39 +1,45 @@
-local status_ok, tree = pcall(require, "nvim-tree")
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
 
-tree.setup({
-  sync_root_with_cwd = true,
-  respect_buf_cwd = true,
-  view = {
-    adaptive_size = true,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = require("nvim-tree.lib").edit },
-        { key = "h", cb = require("nvim-tree.lib").close_node },
-        -- { key = "v", cb = require("nvim-tree.lib").vsplit },
-        -- { key = "s", cb = require("nvim-tree.lib").split },
-        -- { key = "t", cb = require("nvim-tree.lib").tabnew },
-        -- { key = "i", cb = require("nvim-tree.lib").cd },
-        -- { key = "r", cb = require("nvim-tree.lib").refresh },
-        -- { key = "a", cb = require("nvim-tree.lib").create },
-        -- { key = "d", cb = require("nvim-tree.lib").remove },
-        -- { key = "R", cb = require("nvim-tree.lib").rename },
-      },
-    },
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true,
-      window_picker = {
-        enable = false,
-      },
-    },
-  },
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+	return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
+nvim_tree.setup({
+	actions = {
+		open_file = {
+			quit_on_open = true,
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	view = {
+		adaptive_size = true,
+		side = "left",
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+				{ key = "h", cb = tree_cb("close_node") },
+				{ key = "v", cb = tree_cb("vsplit") },
+			},
+		},
+	},
 })
 
 local opts = { noremap = true, silent = true }
--- vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", opts)
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
