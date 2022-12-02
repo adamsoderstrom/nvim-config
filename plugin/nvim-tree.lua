d570d84235802e61aca80acc244e34a25e401a90
Martin Barri <maeertin@gmail.com>
2022-12-02 19:35:15 +0100

    disable focus file on enter

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -23,10 +23,10 @@ nvim_tree.setup({
 		enable = true,
 		show_on_dirs = true,
 	},
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-	},
+	-- update_focused_file = {
+	-- 	enable = true,
+	-- 	update_cwd = true,
+	-- },
 	view = {
 		adaptive_size = true,
 		side = "left",
@@ -43,3 +43,4 @@ nvim_tree.setup({
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+vim.keymap.set("n", "<leader>fe", ":NvimTreeFindFile<CR>", opts)
