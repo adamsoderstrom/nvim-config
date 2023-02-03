71d5fa2570155ede4a6ee04afd26648ce195e765
adamsoderstrom <adamsoderstrom@me.com>
2023-02-03 08:25:03 +0100

    gitsigns: add 'stage_hunk' and 'undo_stage_hunk'

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -5,13 +5,33 @@ end
 
 gitsigns.setup({
   on_attach = function(bufnr)
+    local function map(mode, lhs, rhs, opts)
+        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    end
+
+    -- hunks
+
+    -- -- stage_hunk
+    map('n', '<leader>ghs', ':Gitsigns stage_hunk<CR>')
+    map('v', '<leader>ghs', ':Gitsigns stage_hunk<CR>')
+
+
+    -- -- undo_stage_hunk
+    map('n', '<leader>ghu', ':Gitsigns undo_stage_hunk<CR>')
+    map('v', '<leader>ghu', ':Gitsigns undo_stage_hunk<CR>')
+
+    -- -- reset_hunk
     vim.keymap.set(
       "n",
-      "<leader>ghr",
+     "<leader>ghr",
       ":Gitsigns reset_hunk<CR>",
       { noremap = true, silent = true, buffer = bufnr }
     )
 
+    -- blame
+
+    -- -- blame_line
     vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {
       noremap = true,
       silent = true,
